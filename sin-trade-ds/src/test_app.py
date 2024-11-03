6ec3627b415246ef527254792c01d2974c857158
Adrian Apodaca <aapodaca@shroodbi.com>
2024-11-03 13:49:39 -0800

    docker attempts

--- sin-trade-ds/src/test_app.py
+++ sin-trade-ds/src/test_app.py
@@ -1,8 +1,8 @@
 import pytest
-# `from dsapp import main` is importing the `main` function from the `dsapp` module. This allows you
+# `from app import main` is importing the `main` function from the `app` module. This allows you
 # to use the `main` function in your test script without having to prefix it with the module name
 # every time you call it.
-from dsapp import main
+from app import main
 
 def test_main():
     string = main()
