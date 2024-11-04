8b5a324b9095e4c7ebcff2e31a19ad1c2269ac51
Adrian Apodaca <aapodaca@shroodbi.com>
2024-11-04 08:40:47 -0800

    try again

--- sin-trade-fe/vite.config.ts
+++ sin-trade-fe/vite.config.ts
@@ -1,23 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { defineConfig, loadEnv, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }): UserConfig => {
-  // Load environment variables based on the current mode
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+
+
   const env = loadEnv(mode, process.cwd());
 
-  // Merge the loaded environment variables with process.env
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+
+
   process.env = { ...process.env, ...env };
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+
   return defineConfig({
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+
     plugins: [react()],
     server: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       port: Number(process.env.PORT) || 5173, // Ensure the port is a number
     },
+    preview: {
+      port: Number(process.env.PORT) || 5173, // Ensure the port is a number for preview mode
+    },
   });
-};``
+};
