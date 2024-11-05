a1a82e0125087af4391e489cf252d767fe9052a2
Adrian Apodaca <aapodaca@shroodbi.com>
2024-11-04 21:21:37 -0800

    add initialization of supabase to be

--- sin-trade-be/src/app.py
+++ sin-trade-be/src/app.py
@@ -2,15 +2,36 @@
 from flask import Flask, request
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+import logging
+
+load_dotenv()
 
 app = Flask(__name__)
 # update this to reference environment variable. 
-CORS(app, origins=["http://localhost:5173", "http://localhost:4173",])
+CORS(app, origins=["http://localhost:5173", "http://localhost:4173", "https://sin-trade-fe-14d4ac402398.herokuapp.com/", "https://sin-trade-ds-7ca730853eba.herokuapp.com/", "http:127.0.0.1:5002"])
 port = os.environ.get("PORT", 5002)
 
-print(__name__)
+# initializing supabase instance
+url: str = os.getenv("SUPABASE_URL") 
+key: str = os.getenv("SUPABASE_KEY") 
+
+
+try: 
+    supabase: Client = create_client(url, key)
+    logging("set up supabase")
+except Exception as e:
+    logging.error(f"Failed to connect to database: {e}")
+    supabase = None
+
+print(Client)
 @app.route("/")
 
 
 def main():
     return '{"status":200, "data": "this has been a success"}'
+
+@app.route("/login")
+def login():
+    return "login route"
--- sin-trade-ds/src/app.py
+++ sin-trade-ds/src/app.py
@@ -1,13 +1,20 @@
-
+#!/usr/bin/env python3
 from flask import Flask, request
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-print(__name__)
-port = os.environ.get("PORT", 5002)
-
+# update this to reference environment variable. 
+CORS(app, origins=["http:127.0.0.1:5002", "https://sin-trade-be-51f29d2bbbde.herokuapp.com/"])
+port = os.environ.get("PORT", 5004)
 
 @app.route("/")
 
+
 def main():
     return '{"status":200, "data": "you have made a successful call"}'
+
+# @app.route("/login")
+
+
+# def login()
