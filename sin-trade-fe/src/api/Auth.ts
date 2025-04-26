45593928dbf232d12c6d9195d0adfe8bbfc5afda
Adrian Apodaca <aapodaca@shroodbi.com>
2025-04-26 16:58:56 -0700

    add console log for debugging

--- sin-trade-fe/src/api/Auth.ts
+++ sin-trade-fe/src/api/Auth.ts
@@ -20,6 +20,8 @@ export const login = async ({
 }) => {
   setIsLoading(true);
   try {
+    console.log("DATA URL", dataUrl);
+
     const response = await fetch(`${dataUrl}auth/login`, {
       headers: {
         "Content-Type": "application/json",
